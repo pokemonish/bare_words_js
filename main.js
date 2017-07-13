c731cea19de9b683ae96dfe880ead86e4bd254c2
wasiher <watashiwaher@gmail.com>
2017-07-14 02:02:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ try {
     let self = this
     window.bareWords = new Proxy({}, {
         has: function(target, name) {
-            return true
+            return !(name in self)
         },
         get: function(target, name) {
-            return name in self ? self[name] : name
+            return name
         },
     })
 } catch(e) {
     console.error('Your browser doesn\'t support bare words.')
-}
-with(bareWords) {
-    console.log(we, can, use, bare, words)
 }
